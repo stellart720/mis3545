5b2909f553b8ffa54a3260cb8462a054bf197d91
Zhi Li <lizhiblack@gmail.com>
2016-09-21 14:16:56 -0400

    add S07 exercises

---
+++ SQL/Session_07_SQL_Exercises.sql
@@ -0,0 +1,16 @@
+USE AdventureWorks2012; /*Set current database*/
+
+
+/*1, Display the total amount collected from the orders for each order date. */
+
+/*2, Display the total amount collected from selling the products, 774 and 777. */
+
+/*3, Write a query to display the sales person BusinessEntityID, last name and first name of all the sales persons and the name of the territory to which they belong.*/
+
+/*4,  Write a query to display the Business Entities of the customers that have the 'Vista' credit card.*/
+
+/*5, Write a query to display all the country region codes along with their corresponding territory IDs*/
+
+/*6, Find out the average of the total dues of all the orders.*/
+
+/*7, Write a query to report the sales order ID of those orders where the total value is greater than the average of 
