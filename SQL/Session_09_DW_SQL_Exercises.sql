8b0941977860139c3f529bc61ded0a09562f67bd
Zhi Li <zli@babson.edu>
2016-09-27 12:28:16 -0400

    add DW SQL Exercise

---
+++ SQL/Session_09_DW_SQL_Exercises.sql
@@ -0,0 +1,23 @@
+use AdventureWorksDW2012;
+
+/*total Amount of off-line sales in Massachusetts*/
+
+
+/*Sales Amount of Internet Sales in 1st quarter each year in each country*/
+
+
+
+/*Employees whose birthday is in Feburary*/
+
+
+
+/*who are the Sales Representatives whose birthday is in Feburary?*/
+
+
+
+
+/*List all the sales processed by these Sales Representatives */
+
+
+
+/*who is a better sales representative that was born in Feburary?*/
