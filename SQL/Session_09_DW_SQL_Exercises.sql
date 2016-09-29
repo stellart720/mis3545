dfbfcd4a5ac0ef5d17518d511e3d886bbe15846f
Zhi Li <lzblack@users.noreply.github.com>
2016-09-29 09:37:26 -0400

    Update Session_09_DW_SQL_Exercises.sql

--- SQL/Session_09_DW_SQL_Exercises.sql
+++ SQL/Session_09_DW_SQL_Exercises.sql
@@ -1,11 +1,5 @@
 use AdventureWorksDW2012;
 
-/*total Amount of off-line sales in Massachusetts*/
-
-
-/*Sales Amount of Internet Sales in 1st quarter each year in each country*/
-
-
 
 /*Employees whose birthday is in Feburary*/
 
@@ -21,3 +15,11 @@ use AdventureWorksDW2012;
 
 
 /*who is a better sales representative that was born in Feburary?*/
+
+
+
+
+/*total Amount of off-line sales in Massachusetts*/
+
+
+/*Sales Amount of Internet Sales in 1st quarter each year in each country*/
