8f295fb72fac70b7de60a8adcc6ce63edb5913dd
Zhi Li <lizhiblack@gmail.com>
2016-10-05 12:10:02 -0400

    add SSDT sample

---
+++ SQL/SSDT_CreateSampleDb.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE SampleDb
+Go
+
+USE SampleDb
+Go
+
+--  Create the customer and order tables
+CREATE TABLE Customer(
+	CustomerID Bigint NOT NULL PRIMARY KEY,
+	FirstName varchar(50) NOT NULL,
+	LastName varchar(50) NOT NULL,
+	CustomerRanking varchar(50) NULL)
+
