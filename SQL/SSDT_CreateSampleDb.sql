f754fe9f131bac1578af87a2cbaede6b7a57ee9c
Zhi Li <zli@babson.edu>
2016-10-05 17:58:44 -0400

    update sampledb

--- SQL/SSDT_CreateSampleDb.sql
+++ SQL/SSDT_CreateSampleDb.sql
@@ -6,8 +6,20 @@ Go
 
 --  Create the customer and order tables
 CREATE TABLE Customer(
-	CustomerID Bigint NOT NULL PRIMARY KEY,
+	CustomerID bigint NOT NULL PRIMARY KEY,
 	FirstName varchar(50) NOT NULL,
 	LastName varchar(50) NOT NULL,
 	CustomerRanking varchar(50) NULL)
 
+CREATE TABLE OrderHeader(
+	OrderHeaderID bigint NOT NULL,
+	CustomerID bigint NOT NULL,
+	OrderTotal money NOT NULL)
+
+-- Create the relationship
+ALTER TABLE OrderHeader ADD CONSTRAINT FK_OrderHeader_Customer
+FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID)
+
+-- Add a few Customers
+INSERT INTO Customer (CustomerID, FirstName, LastName, CustomerRanking) VALUES
+(1, 'John', 'Lennon', NULL),
