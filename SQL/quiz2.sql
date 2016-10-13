bed753f097124a1000aa25bc4de7136c2c6221e4
Zhi Li <zli@babson.edu>
2016-10-13 13:19:03 -0400

    add quiz2

---
+++ SQL/quiz2.sql
@@ -0,0 +1,14 @@
+use AdventureWorksDW2012;
+
+
+/*a.	Show total online order quantities (sum of OrderQuantity), date and product name by every day by every product in 2006. */
+
+
+/*b.	Based on a, perform an OLAP operation: roll-up. Also, describe how the roll-up is performed in the comment block.*/
+
+
+
+/*c.	Based on a, perform an OLAP operation: slice. Also, describe how the slicing is performed in the comment block.*/
+
+
+/*d.	Find out the relationship between quantity of orders and one character of customers (for example, the number of kids or cars.) You don’t need to show the equation, but you need to describe what relationship you want to discover in the comment block.*/
